414f502b151173f30f497b506cf4e4a3f4a9331b
Mehmet <mhmtsytrk@gmail.com>
2021-10-06 16:10:37 -0400

    Initial Commit - Cucumber Framework

---
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -0,0 +1,32 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.utility.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+
+    @Before
+    public void setupDriver(){
+
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Driver.getDriver().manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(){
+
+        Driver.closeBrowser();
+    }
+
+
+
+
+
+
+
+
+}
